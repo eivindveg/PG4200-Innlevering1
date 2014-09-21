9be712c7c1bfb9a28413f37f47f8fe09a393ee4f
eivindveg <eivindveg@hotmail.com>
2014-09-21 15:12:42 +0200

    Document

--- default/InputScanner.java
+++ default/InputScanner.java
@@ -1,3 +1,11 @@
 public interface InputScanner<T> {
+
+    /**
+     * Scans the source T for a target String. The implementation should provide its own spec for how to scan what.
+     *
+     * @param source The source possible containing target
+     * @param target The target String possible found in the source
+     * @return Whether or not the source contains target
+     */
     boolean scan(final T source, final String target);
 }
